fbdd9071f1e43ec04b77d332411e2795a6f72fb7
sr-henry <campiottihenrique@gmail.com>
2020-01-31 18:40:55 -0300

    models recognition

--- _aimbot/models/generate_td.py
+++ _aimbot/models/generate_td.py
@@ -4,11 +4,13 @@ import os
 import random
 import joblib
 
-DATADIR = "C:/Users/henri/Desktop/models/"
+DATADIR = os.getcwd()
+
+print("Searching categories folder in { %s }"%DATADIR)
 
 categories = ["ct", "tr"]
 
-im_size = 100
+im_size = 50
 
 training_data = []
 
@@ -18,10 +20,10 @@ for i, cat in enumerate(categories):
         try:
             img_array = cv2.imread(os.path.join(path, img))
             img_array = cv2.resize(img_array, (im_size, im_size))
-            training_data.append([img_array, i])
+            training_data.append((img_array, i))
         except Exception as err:
             print(err)
 
 random.shuffle(training_data)
 
-joblib.dump(training_data, "training_data")
+joblib.dump(training_data, "data_set")
